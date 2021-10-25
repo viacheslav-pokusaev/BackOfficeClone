e075e89810ac8bcfe8d904eff97dcbc8e0f0ef1c
Даниил Калинин <42569208+kdaniilm@users.noreply.github.com>
2021-10-25 15:35:59 +0300

    Update constants.ts

--- Application/ClientApp/app/constants/constants.ts
+++ Application/ClientApp/app/constants/constants.ts
@@ -1,12 +1,19 @@
-﻿export const SPINNER_COFIG_CONST_OBJ = {
+﻿//for user-table.component
+export const SPINNER_COFIG_CONST_OBJ = {
     SIZE: "3rem",
     COLOR: "#1574b3"
 }
-
 export const QUERY_MODEL_CONST_OBJ = {
     TAKE: null,
     LOADING: true
 
 }
-
 export const FROM_COMPONENT = "component";
+
+//for navmenu component
+export const ID_ARR = [
+    "homechevron",
+    "formschevron",
+    "uielementschevron",
+    "tableschevron"
+];
